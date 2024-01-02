ac51244d3e187810a65f0d44fa4fd957dc4e523d
Omochice <mochice.mls.ntl@gmail.com>
2024-01-02 19:55:56 +0900

    feat!: add newline

--- denops/@tataku/emitter/nvim_floatwin.ts
+++ denops/@tataku/emitter/nvim_floatwin.ts
@@ -62,9 +62,12 @@ const emitter = (denops: Denops, option: unknown) => {
   }
   return new WritableStream<string[]>({
     write: async (chunk: string[]) => {
+      const lines = chunk.join("")
+        .trimEnd() // NOTE: to trim last newline
+        .split(/\r?\n/);
       await denops.call(
         "tataku#emitter#nvim_floatwin#open",
-        chunk,
+        lines,
         {
           // @ts-ignore: workaround
           border: resolveBorder(option.border ?? defaults.border),
